45a47f0ae034ce71c92d1d10043c03295d7c5845
taofiksafrudin <taofiksafrudin4@gmail.com>
2019-10-30 02:05:04 +0700

    latihan

---
+++ latihan_pengenalan diri.py
@@ -0,0 +1,9 @@
+namalengkap = "taofiksafrudin"
+namapanggilan = "tofik"
+npm = "311910527"
+tempatlahir = "indramayu"
+telfon = "0895638713383"
+umur = "19"
+alamat = "cikarang, sukatanai"
+
+print("assalamualaikum, \n let me introduce my self. my name is",namalengkap,",\n but you can call me",namapanggilan,".\n my npm is",npm,". I was born in",tempatlahir,"and i am ",umur,"year old. I am very glad if you want to invite my house in",alamat,".\n so, don't forget to call me before the number",telfon,"\n Thank you")
